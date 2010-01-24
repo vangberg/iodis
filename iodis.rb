dceadc93773d0c9754349facb462aea75c981c03
Harry Vangberg <harry@vangberg.name>
2010-01-24 10:12:21 +0100

    more and more

--- iodis.rb
+++ iodis.rb
@@ -23,13 +23,16 @@ Iodis := Object clone do(
     responseType switch(
       "+",  line,
       ":",  line asNumber,
-      "$",  socket readBytes(line asNumber) clone,
-      "*",  values := list()
-            line asNumber repeat(
-              values push(readReply)
-              socket readBytes(2)
-            )
-            values
+      "$",  if (line asNumber < 0, nil,
+                data := socket readBytes(line asNumber)
+                socket readBytes(2)
+                data),
+      "*",  if (line asNumber < 0, nil,
+                values := list()
+                line asNumber repeat(
+                  values push(readReply)
+                )
+                values)
     ) 
   )
 
@@ -50,6 +53,12 @@ Iodis := Object clone do(
   incr := method(key, inlineCommand("INCR", key))
   decr := method(key, inlineCommand("DECR", key))
 
+  lpush := method(key, value, bulkCommand("RPUSH", key, value))
+  lrange := method(key, start, last,
+    inlineCommand("LRANGE", list(key, start, last))
+  )
+  rpop := method(key, inlineCommand("RPOP", key))
+
   ping := method(inlineCommand("PING"))
 )
 
@@ -62,3 +71,25 @@ i get("y0") print
 "\n" print
 "\n" print
 i mget(list("y0", "broke")) foreach(x,x print)
+
+"\n" print
+"lol\n" print
+i get("aksldfhasdkfhasdf") type print
+
+"\n" print
+i mget(list("aisudfhaisdfh", "y0")) print
+
+"\n" print
+i lpush("stupidz", "yep") print
+"\n" print
+i lpush("stupidz", "eehhh") print
+"\nlrange exist:\n" print
+i lrange("stupidz", 0, -1) print
+"\nlrange non-exist:\n" print
+i lrange("stupsadfidz", 0, -1) print
+
+"\n" print
+i lpush("thisisalmost","haha")
+i rpop("thisisalmost") print
+"\nlrange empty:\n" print
+i lrange("thisisalmost", 0, -1) print
